49089e664d82011d47dd582b3e73d2502e78cffb
Smit <smitdesai2130@gmail.com>
2021-04-21 18:47:19 +0530

    first commit🚀

--- 01-Fundamentals-Part -1/index.js
+++ 01-Fundamentals-Part -1/index.js
@@ -1,3 +1,34 @@
 let user = 'Smit';
-if (user === 'Smit') alert('Welcome to the Fight Club!!!');
+// if (user === 'Smit') alert('Welcome to the Fight Club!!!');
 console.log(40 + 8 + 23 + 47 - 89);
+let firstName = 'Messi';
+
+//Boolean
+let javascriptIsFun = true;
+console.log(javascriptIsFun); //true
+
+//typeOf
+console.log(typeof true); //boolean
+console.log(typeof javascriptIsFun); //boolean
+console.log(typeof 'Ronaldo'); //string  quotes important
+console.log(typeof user); //string
+
+//var - redeclare✅ redefine✅
+//let - redeclare❌ redefine✅
+//const - redeclare❌ redefine❌
+
+//Dynamic Typing
+//Do not use let again as it cannot be redeclared but can be REDEFINED
+javascriptIsFun = 'I know right!';
+console.log(typeof javascriptIsFun); //string<--boolean
+
+let year;
+console.log(year); //undefined
+console.log(typeof year); //undefined
+//But typeof null shows as object -- A BUG in JS
+console.log(typeof null);
+
+//Redefining the year
+year = 2021;
+// console.log(year);
+console.log(typeof year); //number
