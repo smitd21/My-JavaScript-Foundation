965c4938bfe8d56d02c1875cc753da1b6c213e07
Smit <smitdesai2130@gmail.com>
2021-04-28 19:21:32 +0530

    THE SWITCH STATEMENT ✅

--- 01-Fundamentals-Part -1/index.js
+++ 01-Fundamentals-Part -1/index.js
@@ -601,3 +601,39 @@ if (hasDriverLicense && hasGoodVision) {
 }
 */
 //----------------------------------------------------------------------------------------
+
+let day = prompt('Enter a day to know your schedule');
+if (day === 'monday') {
+  console.log(`Plan course structure`);
+  console.log(`Go to coding meetup`);
+} else if (day === 'tuesday') {
+  console.log(`Prepare theory videos`);
+} else if (day === 'wednesday' || day === 'thursday') {
+  console.log(`Write code examples`);
+} else if (day === 'friday') {
+  console.log(`Record videos`);
+} else if (day === 'saturday' || day === 'sunday') {
+  console.log(`Enjoy the weekend`);
+} else {
+  console.log(`Not a valid day`);
+}
+//* THE SWITCH STATEMENT
+switch (day) {
+  case 'monday':
+    console.log(`Plan course structure`);
+    console.log(`Go to coding meetup`);
+    break;
+  case 'tuesday':
+    console.log(`Prepare theory videos`);
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log(`Write code examples`);
+    break;
+  case 'friday':
+    console.log(`Record videos`);
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log(`Enjoy the weekend`);
+}
