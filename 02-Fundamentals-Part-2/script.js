6b7df985a4185f38025b75c60bf6942bac1dc3b9
Smit <smitdesai2130@gmail.com>
2021-05-01 08:28:44 +0530

    Function Declarations vs Expressions ✅

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -31,6 +31,10 @@ logger();
 logger();
 logger(23); //No parameters are taken by this function even if you pass it does'nt matter it'll run same as above
 
+/*
+! FUNCTION produces a value so always it should be return
+(actually in JS functions are just values)
+
 let customerChoice = prompt(`What type of juice you prefer ?`);
 
 //Juice maker
@@ -56,7 +60,7 @@ switch (customerChoice) {
   default:
     console.log('So sorry :) We have only cocktail, orange and apple juice!!');
 }
-
+*/
 //Clean code principle : Do not repeat yourself or dry
 //Functions are perfect for implementing dry code as they're reusable code blocks
 
@@ -64,10 +68,48 @@ switch (customerChoice) {
 
 //Assignment 1 ✅
 function describeCountry(country, population, capitalCity) {
-  const countryInfo = `'${country} has ${population} million people and its
-    capital city is ${capitalCity}'`;
+  const countryInfo = `${country} has ${population} million people and its
+    capital city is ${capitalCity}`;
   return countryInfo;
 }
 console.log(describeCountry('India', 132, 'Delhi'));
 console.log(describeCountry('Finland', 6, 'Helsinki'));
 console.log(describeCountry('United States', 32, 'Washington D.C.'));
+
+//---------------------------------------------------------------------------------------------
+
+//* Function Declarations vs Expressions
+
+// ---- function declaration ---- The functioncan be called before declaration also
+function calcAge1(birthYear) {
+  //return 2021-birthYear;
+  const currentYear = 2021;
+  const age = currentYear - birthYear;
+  return age; //! return value always is important
+}
+console.log(`Your age is :`, calcAge1(2000));
+
+// ---- function expression ---- The function cannot be called before declaration
+
+//(actually in JS:  functions are just values so if it's a value, we can store it in a variable right !!) so storing it in a variable....
+const calcAge2 = function (birthYear) {
+  return 2021 - birthYear;
+};
+console.log(`Your age is: `, calcAge2(1997));
+
+//or
+
+const calcAge3 = (birthYear) => {
+  return 2021 - birthYear;
+};
+console.log(`Your age is :`, calcAge3(2004));
+
+/*
+! Difference : 
+You can call a function declaration (calcAge1) before they are defined in the code 
+but function expression(calcAge2 & calcAge3) cannot be called before declaring - Cannot access 'calcAge2' before initialization
+
+And internally this happens because of a process called 'HOISTING' - that will study later
+
+*Preference to use Function expression (calcAge2 or calcAge3) - coz it should be call only after defining and then calling
+*/
