4ab9ef4b810bc7edc8e1f32c8063d2bb4eb60acc
smitd21 <smitdesai2130@gmail.com>
2022-01-02 21:09:37 +0530

    Starting after long: Day 0 in Js Tips

--- JavaScript Tips/script.js
+++ JavaScript Tips/script.js
@@ -6,3 +6,12 @@ If any of the value is of string type and it's added to other values
 JS will first automatically convert the  other values to a string 
 And then they are altogether concatenated giving an entire string as output
 */
+const javascript = 'fun';
+console.log(`JavaScript is indeed ${javascript}`);
+
+const haveMoney = true;
+if(haveMoney){
+    console.log(`Buy a Lamborghini !!... And that's how you should live!!`);
+} else{
+    console.log(`Go by train ;(`);
+}
